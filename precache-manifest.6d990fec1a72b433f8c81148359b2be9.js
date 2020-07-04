20c1f84662777405f46d7a681c838d03aca70a8e
Ashik80 <ashikurrahman020995@gmail.com>
2020-07-05 02:58:29 +0600

    Updates

---
+++ precache-manifest.6d990fec1a72b433f8c81148359b2be9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadde73354f95e0f7bd24a9d39615b53",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "fcf2576f3b2fee0542e7",
+    "url": "/Portfolio/static/css/main.c1a9844e.chunk.css"
+  },
+  {
+    "revision": "62613f2a200eac218eb2",
+    "url": "/Portfolio/static/js/2.38c1df37.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.38c1df37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcf2576f3b2fee0542e7",
+    "url": "/Portfolio/static/js/main.eae11ffc.chunk.js"
+  },
+  {
+    "revision": "f857b15ea327607669be",
+    "url": "/Portfolio/static/js/runtime-main.bcfc9a4f.js"
+  },
+  {
+    "revision": "d45df73dcdf6c491bfa16414055bc7a4",
+    "url": "/Portfolio/static/media/ashik.d45df73d.jpg"
+  },
+  {
+    "revision": "8d17a17a162622f0906456eba84071ca",
+    "url": "/Portfolio/static/media/development.8d17a17a.jpg"
+  },
+  {
+    "revision": "b144afc73492c4b64e374e31f8098804",
+    "url": "/Portfolio/static/media/palette.b144afc7.jpg"
+  }
+]);
